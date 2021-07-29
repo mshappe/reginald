1c4598d911f09e2d50a6522cc7cf28cc059dab75
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2021-07-28 21:39:17 -0500

    Accept (and require) name for user)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,4 +38,5 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of :email }
   it { is_expected.to validate_uniqueness_of :email }
+  it { is_expected.to validate_presence_of :name }
 end
