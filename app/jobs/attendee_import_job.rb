0d385333a27f23faf7328322e818399dcc19499c
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2021-08-01 14:33:58 -0500

    Deliver notifications immediately

--- app/jobs/attendee_import_job.rb
+++ app/jobs/attendee_import_job.rb
@@ -5,8 +5,8 @@ class AttendeeImportJob < ApplicationJob
 
   def perform(file, user)
     AttendeeImportService.new.import(file)
-    AttendeeImportMailer.success(file, user).deliver_later
+    AttendeeImportMailer.success(file, user).deliver_now
   rescue StandardError => e
-    AttendeeImportMailer.failure(file, user, e).deliver_later
+    AttendeeImportMailer.failure(file, user, e).deliver_now
   end
 end
